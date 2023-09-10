6520a07ed1af52296494a40ea1b8243b7f8c66a5
Pakhi-Sharma16 <144621277+Pakhi-Sharma16@users.noreply.github.com>
2023-09-11 03:12:53 +0530

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -19,3 +19,60 @@ if Y%X==0:
   print("Y is divisible by X")
 else :
   print("Y is not divisible by X")  
+
+4.
+a=int(input("enter a number: "))
+if a%2==0:
+  print("a is even")
+else:
+  print("a is odd")
+
+5.
+r=int(input("enter radius:"))
+if 1<r<100:
+  print("area=" , 3.14*(r**2))
+else:
+  print("invalid input")
+
+6.
+C= float(input("enter temp in C:"))
+F=(C*1.8)+32
+K=C+273.15
+print(C,"in fahreheit is", F, "and" ,C , "in kelvin is",K)
+
+7.
+C= float(input("enter temp in C:"))
+F=(C*1.8)+32
+print("C=F at -40 degree celsius")
+
+8.
+Year=int(input("enter a year:"))
+if Year%400==0 or Year%100!=0 and Year%4==0:
+  print("it is a leap year")
+else:
+  print("it's not a leap year")
+
+9.
+money=float(input("enter the monthly investment:"))
+time=float(input("enter duration in months:"))
+rate=7.1
+if money>=500 and time>=6:
+  interest = float(money*(rate/time))
+  print(interest)
+elif time<6:
+  print("duration should be more than 6 months")
+elif money<500:
+  print("minimum amt should be more than Rs.500")
+else:
+  print("invalid input")
+
+10.
+n=int(input("enter the time in seconds:")) #seconds
+if n in range(1,86400):
+  hours=int(n/3600)
+  n%=3600
+  minutes=int(n/60)
+  n%=60
+  print(hours,minutes,n)
+else:
+  print("invalid input")
