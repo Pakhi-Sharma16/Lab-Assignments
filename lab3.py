97a3636e3acf0b76d1d63eb2703f4dd2856eeb48
Pakhi-Sharma16 <144621277+Pakhi-Sharma16@users.noreply.github.com>
2023-09-19 11:59:12 +0530

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -7,3 +7,58 @@ print(area)
 
 
 2.
+weight=float(input("enter the weight in kg:")) #get weight in kg
+height=float(input("enter the height in m:")) #get height in m
+BMI= weight/(height**2)#calculate BMI
+print(BMI)
+weight=float(input("Enter weight in pounds:"))
+height=float(input("Enter height in inches:"))
+#converting
+#calculate BMI
+BMI=(703*weight)/(height**2)
+print(BMI)
+
+3.
+a=int(input("Enter side1:"))
+b=int(input("Enter side2:"))
+c=int(input("Enter side3:"))
+if (a+b)>c:
+    print("Yes, They does form the sides of a triangle")
+elif (a+c)>b:
+    print("Yes")
+else:
+    print("No, They does not form a triangle")
+
+
+4.
+a=int(input("enter a three digit number:"))
+b= a//100
+c=(a//10)%10
+d=a%10
+sum=b+c+d
+print(sum)
+if sum%3==0:
+    print("it is divisible by 3")
+else:
+    print("it is not divisible by 3")
+
+
+5.
+x=int(input("enter a five digit number:"))
+a= a//10000
+b=(a//1000)%10
+c=(a//100)%10
+d=(a//10)%10
+e=a%10
+print(e,d,c,b,a)
+print("numbers are palindrome")
+
+
+6.
+
+
+
+
+7.
+
+
